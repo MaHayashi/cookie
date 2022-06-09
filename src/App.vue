0745f10fa11c19fc0824184c240a4afc3c4443ac
Ma Hayashi <matudxs@gmail.com>
2022-06-09 11:18:16 -0300

    Criando a page cookie com Vue.JS

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app"> <!-- Sempre com DIV-->
+    <img alt="Cookie" src="./assets/cookie.jpeg">
+    <HelloCookie msg="Cookie"/>
+    <WannaCookie></WannaCookie>
+  </div> 
+</template>
+
+<script>
+import HelloCookie from './components/HelloCookie.vue'
+import WannaCookie from './components/WannaCookie.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloCookie, WannaCookie
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/WannaCookie.vue
@@ -0,0 +1,34 @@
+<template>  
+  <form>
+    <div id="emailHelp" class="form-text">Acesse o fantástico mundo dos Cookies</div>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">E-mail </label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Senha </label>
+      <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="mb-4 form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1"> News?</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Entre</button>
+  </form>
+</template>
+
+<style scoped>
+.form-text{
+  margin: 40px 0 10px;
+  font-weight: bolder;
+}
+.mb-3{
+  margin: 5px 0 0;
+}
+.mb-4{
+  margin: 10px 0px 5px;
+}
+button{
+  margin: 10px 0 0;
+}
+</style>
